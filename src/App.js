b607dd24dfdb644fd4222939bf761b093bf70773
Tung Le <tunglungtung87@gmail.com>
2022-03-11 14:04:12 +0700

    adding new collection items and collection page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-/// Create example Component to using Route
-const HatsPage = () => {
-  return(
-    <div>
-      <h1>Hello Hats</h1>
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact="true" path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,7 @@ import './menu-item-styles.scss';
 import {withRouter} from 'react-router-dom';
 
 /// Component dont hold any state => using functional component instead
-const MenuItem = ({title,imageUrl, size, history, linkUrl, match}) => {
+const MenuItem = ({title,imageUrl, size, linkUrl, history, match}) => {
   return(
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div className='background-image' style={{ backgroundImage: `url(${imageUrl})`}} />
@@ -16,4 +16,5 @@ const MenuItem = ({title,imageUrl, size, history, linkUrl, match}) => {
   );
 }
 
+/// use withRouter (High Order Component) to return component you can access to history, match
 export default withRouter(MenuItem);
